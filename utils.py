4fa985529e1d80b305b11ad19719c6a932ec215f
arkodasgupta0412 <arkodasgupta05@gmail.com>
2023-08-13 19:59:32 +0530

    Add intro and rules function

---
+++ utils.py
@@ -0,0 +1,12 @@
+from time import sleep
+import random
+
+def intro():
+
+    print("Welcome to PIG dice-game")
+    print("Play with your friends!")
+
+
+def rules():
+
+    print("Win by achieving a target score of 50 or outperform your friends!")
