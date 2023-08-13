d676daecb9f015e27f15e9bf29faa5a4a3b1e512
arkodasgupta0412 <arkodasgupta05@gmail.com>
2023-08-13 20:07:11 +0530

    Add user_input function

--- utils.py
+++ utils.py
@@ -10,3 +10,23 @@ def intro():
 def rules():
 
     print("Win by achieving a target score of 50 or outperform your friends!")
+
+
+def user_input():
+
+    while True:
+
+        number_of_players = input("Enter number of players (2-4): ")
+
+        if number_of_players.isdigit():
+            
+            if 2 <= int(number_of_players) <= 4:
+                break
+
+            else:
+                print("Must be between 2 and 4")
+        
+        else:
+            print("Invalid! Please try again")
+
+    return number_of_players
